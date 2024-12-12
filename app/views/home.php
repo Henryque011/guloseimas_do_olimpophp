8219a849502052fee25305d2fece0d3289f2e506
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2024-12-11 22:23:45 -0300

    home

--- app/views/home.php
+++ app/views/home.php
@@ -1,3 +1,5 @@
+<!-- <h1><?php echo $mensagem ?></h1> -->
+<!-- <h2><?php echo $nome ?></h2> -->
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -7,13 +9,13 @@
     <title>Guloseimas Do Olimpo</title>
     <meta name="author" content="©SenaCria">
     <meta http-equiv="X-UA-Compatible" content="ie=edge"> <!-- Compatibilidade com o EDGE -->
-    <link rel="shortcut icon" href="public/assets/img/logo_header.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo BASE_URL ?>assets/img/logo_header.svg" type="image/x-icon">
 
-    <link rel="stylesheet" type="text/css" href="public/vendors/slick/slick.css" />
-    <link rel="stylesheet" type="text/css" href="public/vendors/slick/slick-theme.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL ?>vendors/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL ?>vendors/slick/slick-theme.css" />
 
-    <link rel="stylesheet" href="public/assets/css/style.css">
-    <link rel="stylesheet" href="public/assets/css/reset.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL ?>assets/css/reset.css">
 
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-CB1WZZ9KRY"></script>
