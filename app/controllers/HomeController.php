7019d9241d101f0b6bfd1ee49f27b063a521b448
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2024-12-11 22:24:04 -0300

    homecontroller

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,7 +1,9 @@
 <?php 
+
 class HomeController extends Controller{
 
     public function index(){
+        
         $dados = array();
 
         $dados['mensagem'] = 'Bem-vindo a guloseimas do olimpo';
