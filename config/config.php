3bc7d0fc859806813aa510056ef0ab56fadb3e6b
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2024-12-11 22:23:38 -0300

    config

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ if(session_status() == PHP_SESSION_NONE){
 }
 
 // define("BASE_URL","http://localhost/guloseimas_do_olimpophp/public");
-// define("BASE_URL","http://localhost/guloseimas_do_olimpophp/guloseimas_do_olimpophp/public/");
+define("BASE_URL","http://localhost/guloseimas_do_olimpophp/guloseimas_do_olimpophp/public/");
 
 // define("DB_HOST", "smpsistema.com.br");
 // define("DB_NAME", "u283879542_henryque");
@@ -21,7 +21,7 @@ define("DB_PASS", "Henryque@tipi02");
 spl_autoload_register(function ($classe){
     if(file_exists('..app/controllers'. $classe . '.php')){
         require_once '../app/controllers/' . $classe . '.php';
-        //var_dump('../app/controllers/'. $classe .'.php');
+        // var_dump('../app/controllers/'. $classe .'.php');
     }
 
     if(file_exists('../app/models/'. $classe . '.php')){
