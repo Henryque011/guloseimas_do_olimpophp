2ee143f2fdff9955d28a5609fc3f87f489ad8a9c
henryque <henryquenonatosilva@gmail.com>
2025-04-24 09:31:33 -0300

    api

---
+++ app/views/api.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guloseimas Do Olimpo</title>
+    <meta name="author" content="©SenaCria">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> <!-- Compatibilidade com o EDGE -->
+    <link rel="shortcut icon" href="http://localhost/guloseimas_do_olimpophp/public/assets/img/logo_header.svg" type="image/x-icon">
+
+    <link rel="stylesheet" type="text/css" href="http://localhost/guloseimas_do_olimpophp/public/vendors/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="http://localhost/guloseimas_do_olimpophp/public/vendors/slick/slick-theme.css" />
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://localhost/guloseimas_do_olimpophp/public/assets/css/style.css">
+    <link rel="stylesheet" href="http://localhost/guloseimas_do_olimpophp/public/assets/css/reset.css">
+    <!-- Bootstrap CSS -->
+
+
+    <!-- Bootstrap -->
+
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CB1WZZ9KRY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-CB1WZZ9KRY');
+    </script>
+
+
+</head>
+
+<body>
+
+    <div class="mensagem">
+        <h1>✅ Cheguei aqui!</h1>
+        <p>Esta é a view da API. A rota está funcionando corretamente.</p>
+    </div>
+
+</body>
+
+</html>
