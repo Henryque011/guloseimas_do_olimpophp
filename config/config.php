3774c245f8a0a1eb3b85eddd836586297b69ec7c
henryque <henryquenonatosilva@gmail.com>
2025-05-20 08:40:25 -0300

    config - definição de dados para recuperar a senha via email

--- config/config.php
+++ config/config.php
@@ -5,7 +5,6 @@ if(session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-
 // Definir URL  da aplicação 
 // define("BASE_URL" ,"https://kioficina.smpsistema.com.br/");
 // define("BASE_URL" ,"http://localhost/guloseimas_do_olimpophp/public/");
@@ -22,29 +21,28 @@ define("DB_NAME", "u283879542_olimpo");
 define("DB_USER", "u283879542_olimpo");
 define("DB_PASS", "Senac@olimpo01");
 
+// Recuperar Senha 
+define("EMAIL_HOST", "smpsistema.com.br");
+define("EMAIL_PORT", "465");
+define("EMAIL_USER", "abxqtzseven@gmail.com");
+define("EMAIL_PASS","mvll lewe mtxj ugeb");
+
 // //  Configuração do Data Base para desenvolvimento local
 //  define("DB_HOST", "193.203.175.197"); // Ou 127.0.0.1
 //  define("DB_NAME", "u230564252_olimpo"); // Substitua pelo nome do banco local
 //  define("DB_USER", "u230564252_olimpo"); // Usuário padrão do MySQL local
 //  define("DB_PASS", "21566647aA#"); // Geralmente a senha do root está em branco
 
-
 // define("DB_HOST", "smpsistema.com.br");
 // define("DB_NAME", "u283879542_alex");
 // define("DB_USER", "u283879542_alex");
 // define("DB_PASS", "Alex@tipi02");
 
-
 // EMAIIL  // EMAIIL // EMAIIL
-
-
 define('HOTS_EMAIL', 'smtp.gmail.com');
 define('PORT_EMAIL', '465');
 define('USER_EMAIL', 'desenvolvedorweb21@gmail.com');
 define('PASS_EMAIL', 'fgip hdva gvep mdso');
-
-
-
 
 
 
@@ -53,15 +51,7 @@ define('PASS_EMAIL', 'fgip hdva gvep mdso');
 // define('USER_EMAIL', 'desenvolvedorweb21@gmail.com');
 // define('PASS_EMAIL', 'dqay vqmc xpjd yosa');
 
-
-
-
-
-
-
 // EMAIIL // EMAIIL // EMAIIL // EMAIIL
-
-
 
 // Sistema de autoload  das class 
 spl_autoload_register(function ($classe) {
