7e123f39792c9a59ebd8dfe7183ba96077b8ed94
henryque <henryquenonatosilva@gmail.com>
2025-05-21 08:45:41 -0300

    head para tela de recuperação

---
+++ app/views/template/head.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guloseimas do Olimpo</title>
+    <meta name="author" content="©Henryque_Nonato">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="shortcut icon" href="../public/assets/img/logo_header.svg" type="image/x-icon">
+    <link rel="stylesheet" href="../public/assets/css/reset.css">
+    <link rel="stylesheet" href="../public/assets/css/style.css">
+</head>
