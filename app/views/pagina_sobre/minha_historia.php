dced971102ba4ac8bfdba2ffb6a7427ab8c3951d
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-05-31 04:43:15 -0300

    format document

--- app/views/pagina_sobre/minha_historia.php
+++ app/views/pagina_sobre/minha_historia.php
@@ -1,20 +1,15 @@
 <section class="minha_historia">
-            <article class="site">
-                <div>
-                    <h3>MINHA</h3>
-                </div>
-
-                <div class="lado_a_lado">
-
-                    <div class="foto_quem" style="background-image: url('<?php echo BASE_URL . 'uploads/' . htmlspecialchars($minha_historia['foto_galeria'], ENT_QUOTES, 'UTF-8'); ?>');">
-
-                    </div>
-
-                    <div>
-                        <p>Minha jornada na confeitaria começou como uma renda extra, fazendo trufas, até perceber que havia encontrado minha verdadeira paixão. O Guloseimas do Olimpo surgiu dessa transformação de hobby em negócio. Após me formar no Curso Técnico de Confeitaria do Senac, decidi investir nesse caminho.
-
-Hoje, meu foco é criar experiências especiais para cada cliente, oferecendo doces artesanais como brownies, cookies e bolos, sempre com carinho e qualidade. Confeitar para mim é mais do que vender doces – é criar momentos inesquecíveis. ✨</p>
-                    </div>
-                </div>
-            </article>
-        </section>
+    <article class="site">
+        <div>
+            <h3>MINHA</h3>
+        </div>
+        <div class="lado_a_lado">
+            <div class="foto_quem" style="background-image: url('<?php echo BASE_URL . 'uploads/' . htmlspecialchars($minha_historia['foto_galeria'], ENT_QUOTES, 'UTF-8'); ?>');">
+            </div>
+            <div>
+                <p>Minha jornada na confeitaria começou como uma renda extra, fazendo trufas, até perceber que havia encontrado minha verdadeira paixão. O Guloseimas do Olimpo surgiu dessa transformação de hobby em negócio. Após me formar no Curso Técnico de Confeitaria do Senac, decidi investir nesse caminho.
+                    Hoje, meu foco é criar experiências especiais para cada cliente, oferecendo doces artesanais como brownies, cookies e bolos, sempre com carinho e qualidade. Confeitar para mim é mais do que vender doces – é criar momentos inesquecíveis. ✨</p>
+            </div>
+        </div>
+    </article>
+</section>
