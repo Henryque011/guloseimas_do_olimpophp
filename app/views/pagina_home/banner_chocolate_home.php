8936604c6067ef42fd7c0f2b313788d46ba1b3c0
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-05-31 04:19:34 -0300

    fomat document

--- app/views/pagina_home/banner_chocolate_home.php
+++ app/views/pagina_home/banner_chocolate_home.php
@@ -8,13 +8,11 @@
                 <div class="box_carousel">
                     <?php foreach ($produto as $produtos): ?>
                         <?php if ($produtos['status_pedido'] === 'Ativo'): ?> <!-- Verifica se o produto está ativo -->
-                            <div><a href="<?php echo BASE_URL . 'produtos/detalhe/' . $produtos['link_produto'];?>"><img src="<?php echo BASE_URL . 'uploads/' . $produtos['foto_produto']; ?>"
+                            <div><a href="<?php echo BASE_URL . 'produtos/detalhe/' . $produtos['link_produto']; ?>"><img src="<?php echo BASE_URL . 'uploads/' . $produtos['foto_produto']; ?>"
                                         alt="<?php echo htmlspecialchars($produtos['alt_foto_produto']); ?>"></a></div>
-                           
                         <?php endif; ?>
-
                     <?php endforeach; ?>
-                    <?php echo BASE_URL . 'produtos/detalhe/' . $item['link_produto'];?>
+                    <?php echo BASE_URL . 'produtos/detalhe/' . $item['link_produto']; ?>
                 </div>
             </section>
         </section>
