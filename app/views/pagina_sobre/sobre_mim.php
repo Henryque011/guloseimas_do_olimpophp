cb40aba628542a518902c7045b8c9e69021954b6
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-05-31 04:47:04 -0300

    format document

--- app/views/pagina_sobre/sobre_mim.php
+++ app/views/pagina_sobre/sobre_mim.php
@@ -11,7 +11,7 @@
         <div class="txt">
             <p>A Guloseimas do Olimpo nasceu na cozinha da minha mãe, impulsionada pela paixão por doces e pela mitologia. Como fã de Percy Jackson, escolhi um nome que representasse meu objetivo: criar doces divinos, dignos de uma experiência única. O que começou como uma renda extra se tornou um propósito maior – proporcionar momentos especiais com qualidade, autenticidade e carinho.
 
-Nossa produção é artesanal, com ingredientes selecionados e um toque de criatividade para surpreender você. Começamos com trufas e ovos de Páscoa e hoje oferecemos um cardápio variado, incluindo brownies, cookies e bolos. Seja para presentear, celebrar ou simplesmente se deliciar, cada doce é feito com capricho para garantir uma experiência celestial! ✨
+                Nossa produção é artesanal, com ingredientes selecionados e um toque de criatividade para surpreender você. Começamos com trufas e ovos de Páscoa e hoje oferecemos um cardápio variado, incluindo brownies, cookies e bolos. Seja para presentear, celebrar ou simplesmente se deliciar, cada doce é feito com capricho para garantir uma experiência celestial! ✨
             </p>
         </div>
     </article>
