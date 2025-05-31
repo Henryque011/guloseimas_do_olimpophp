9ce4413394d991be78ef8e6f5fbdea7530efbe28
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-05-31 06:55:16 -0300

    foramt document

--- app/views/api.php
+++ app/views/api.php
@@ -8,20 +8,15 @@
     <title>Guloseimas Do Olimpo</title>
     <meta name="author" content="©SenaCria">
     <meta http-equiv="X-UA-Compatible" content="ie=edge"> <!-- Compatibilidade com o EDGE -->
-    <link rel="shortcut icon" href="http://localhost/guloseimas_do_olimpophp/public/assets/img/logo_header.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo BASE_URL; ?>assets/img/logo_header.svg" type="image/x-icon">
 
-    <link rel="stylesheet" type="text/css" href="http://localhost/guloseimas_do_olimpophp/public/vendors/slick/slick.css" />
-    <link rel="stylesheet" type="text/css" href="http://localhost/guloseimas_do_olimpophp/public/vendors/slick/slick-theme.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>vendors/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>vendors/slick/slick-theme.css" />
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="http://localhost/guloseimas_do_olimpophp/public/assets/css/style.css">
-    <link rel="stylesheet" href="http://localhost/guloseimas_do_olimpophp/public/assets/css/reset.css">
-    <!-- Bootstrap CSS -->
-
-
-    <!-- Bootstrap -->
-
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/reset.css">
 
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-CB1WZZ9KRY"></script>
@@ -35,7 +30,6 @@
 
         gtag('config', 'G-CB1WZZ9KRY');
     </script>
-
 
 </head>
 
