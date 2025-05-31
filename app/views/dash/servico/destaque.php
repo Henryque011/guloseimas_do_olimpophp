0213720cec3d0c50833fb6b6827a9dd895c5ccb6
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-05-31 06:14:21 -0300

    fortmat document and add

--- app/views/dash/servico/destaque.php
+++ app/views/dash/servico/destaque.php
@@ -1,27 +1,15 @@
-
-
-
-
-
-
 <style>
   button {
     border: none;
     background-color: transparent;
   }
 
-  .pg_produto{
+  .pg_produto {
     width: 230px;
     height: 230px;
     border-radius: 5px;
   }
 </style>
-
-
-
-
-
-
 
 <table class="table table-hover">
   <thead>
@@ -29,12 +17,11 @@
       <th scope="col">id</th>
       <th scope="col">Foto</th>
       <th scope="col">Nome</th>
-     
+
       <th scope="col">Preço</th>
       <!-- <th scope="col">Tempo</th> -->
       <!-- <th scope="col">Especialidade</th> -->
       <th scope="col">Editar</th>
-    
 
     </tr>
   </thead>
@@ -46,21 +33,19 @@
         <td><img src="<?php echo BASE_URL . 'uploads/' . $linha['foto_produto'] ?>" alt="<?php echo $linha['alt_foto_produto'] ?>" class="pg_produto"></td>
 
         <td><?php echo $linha['nome_produto'] ?></td>
-       
-        <td><?php echo $linha['preco_produto'] ?></td>
 
+        <td><?php echo $linha['preco_produto'] ?></td>
 
         <td>
           <a href="<?php echo BASE_URL . 'produtos/editar/' . $linha['id_produto']; ?>">
             <button><i class="bi bi-pencil-fill"></i></button>
           </a>
         </td>
-      
       </tr>
-
 
     <?php endforeach; ?>
   </tbody>
 </table>
-<script src="http://localhost/guloseimas_do_olimpophp/public/vendors/dash/js/adminlte.js"></script>
+<script src="<?php echo BASE_URL; ?>vendors/dash/js/adminlte.js"></script>
+
 </html>
