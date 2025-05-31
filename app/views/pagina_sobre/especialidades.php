ca08ad298852bf5e938825dcd33f2870785b057d
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-05-31 04:40:19 -0300

    format document

--- app/views/pagina_sobre/especialidades.php
+++ app/views/pagina_sobre/especialidades.php
@@ -1,25 +1,22 @@
 <section class="especialidades">
-            <article class="site">
-                <div>
-                    <h3>ESPECIALIDADES</h3>
-                </div>
-                <div class="lado_a_lado">
-                <?php foreach ($servicos as $servicos_sobre): ?>
-                    <?php if ($servicos_sobre['status_servico'] === 'Ativo'): ?> <!-- Verifica se o produto está ativo -->
+    <article class="site">
+        <div>
+            <h3>ESPECIALIDADES</h3>
+        </div>
+        <div class="lado_a_lado">
+            <?php foreach ($servicos as $servicos_sobre): ?>
+                <?php if ($servicos_sobre['status_servico'] === 'Ativo'): ?>
                     <div class="especialidade_produto">
                         <div class="pd">
                             <img src="<?php echo BASE_URL . 'uploads/' . $servicos_sobre['foto_servico']; ?>" alt="<?php echo htmlspecialchars($servicos_sobre['alt_foto_servico']); ?>">
-
                             <div class="especialidade_informacoes">
                                 <h4><?php echo $servicos_sobre['nome_servico']; ?></h4>
                                 <p><?php echo $servicos_sobre['descricao_servico']; ?></p>
                             </div>
                         </div>
-
                     </div>
-                    <?php endif; ?>
-
-                    <?php endforeach; ?>
-                </div>
-            </article>
-        </section>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
+    </article>
+</section>
