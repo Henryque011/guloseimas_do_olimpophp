02f606abb1a41ca096fa39e6c513e0e25035c451
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-05-31 04:26:13 -0300

    format document and base_url

--- app/views/pagina_home/ben_vindo.php
+++ app/views/pagina_home/ben_vindo.php
@@ -8,18 +8,16 @@
                 <h3>Reserve agora mesmo a nossa
                     deliciosa variedade de doces</h3>
 
-                <a href="http://localhost/guloseimas_do_olimpophp/public/galeria">Veja nossa galeria</a>
+                <a href="<?php echo BASE_URL; ?>galeria">Veja nossa galeria</a>
             </div>
             <div class="lado_a_lado_img">
                 <?php foreach ($galeria as $foto): ?>
                     <div class="img_01">
-                        <a href="http://localhost/guloseimas_do_olimpophp/public/galeria"><img src="<?php echo BASE_URL . 'uploads/' . $foto['foto_galeria']; ?>" class="ben_vindo_img"
+                        <a href="<?php echo BASE_URL; ?>galeria"><img src="<?php echo BASE_URL . 'uploads/' . $foto['foto_galeria']; ?>" class="ben_vindo_img"
                                 alt="<?php echo $foto['alt_foto_galeria']; ?>"></a>
                     </div>
                 <?php endforeach; ?>
-
             </div>
-
         </div>
     </article>
 </section>
