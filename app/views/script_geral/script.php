7592e488b97c102d4c9adee5f4455984bc3ff41b
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-05-31 05:07:12 -0300

    format document and add base_url

--- app/views/script_geral/script.php
+++ app/views/script_geral/script.php
@@ -3,8 +3,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-3.7.1.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="http://localhost/guloseimas_do_olimpophp/public/vendors/slick/slick.min.js"></script>
-<script type="text/javascript" src="http://localhost/guloseimas_do_olimpophp/public/assets/js/animacao.js"></script>
-<script src="http://localhost/guloseimas_do_olimpophp/public/assets/js/mobile-navbar.js"></script>
+<script type="text/javascript" src="<?php echo BASE_URL; ?>vendors/slick/slick.min.js"></script>
+<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/animacao.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/mobile-navbar.js"></script>
 <script src="https://kit.fontawesome.com/a327b26a99.js" crossorigin="anonymous"></script>
-
