c83c7bab840de32f1c2440a3e2c873e623859d41
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-06-22 22:46:50 -0300

    :test_tube: test - test caminho de erro.

--- app/controllers/ErroController.php
+++ app/controllers/ErroController.php
@@ -1,19 +1,15 @@
 <?php
- 
- 
-class ErroController{
- 
- 
-    public function index(){
- 
+
+
+class ErroController
+{
+
+
+    public function index()
+    {
+
         echo 'Esta caindo no ERRO CONTROLLER';
 
-
-        echo '<img src="assets/img/BRIGADEIRO 2.svg" alt="brigadeiro">';
-
-      
-      
+        echo '<img src="<?= BASE_URL ?>assets/img/BRIGADEIRO 2.svg" alt="brigadeiro">';
     }
- 
- 
 }
